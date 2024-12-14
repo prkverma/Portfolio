083fd132fd7d1aef8abab239e93aedecd6234714
prkverma <167280440+prkverma@users.noreply.github.com>
2024-12-14 14:20:06 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const counters = document.querySelectorAll('.stat h3');
+    const speed = 5000; // The lower the slower
+
+    counters.forEach(counter => {
+        const updateCount = () => {
+            const target = +counter.getAttribute('data-target');
+            const count = +counter.innerText;
+            const increment = target / speed;
+
+            if (count < target) {
+                counter.innerText = Math.ceil(count + increment);
+                setTimeout(updateCount, 1);
+            } else {
+                counter.innerText = target;
+            }
+        };
+
+        updateCount();
+    });
+});
